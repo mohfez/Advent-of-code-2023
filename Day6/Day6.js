314c961de8a3505cb0f1c67a4ecd18ab15472652
mohfez <github@github.com>
2023-12-06 12:39:14 +0000

    day6

--- Day6/Day6.js
+++ Day6/Day6.js
@@ -3,14 +3,7 @@ const fs = require("fs");
 let input = fs.readFileSync("Day6/input.txt", { encoding: "utf-8" });
 
 const PT2 = true;
-if (PT2)
-{
-    input = input.split("\r\n").map(part => part.replace(/\s+/g, "")).map(part => part.match(/\d+/g).map(Number));
-}
-else
-{
-    input = input.split("\r\n").map(part => part.match(/\d+/g).map(Number));
-}
+input = input.split("\r\n").map(part => PT2 ? part.replace(/\s+/g, "").match(/\d+/g).map(Number) : part.match(/\d+/g).map(Number));
 
 let ans = 1;
 for (let i = 0; i < input[0].length; i++)
@@ -31,6 +24,6 @@ function FindRange(time, distance)
     // -heldTime^2 + heldTime*time - distance > 0
 
     let a = -1, b = time, c = -distance;
-    let discriminant = Math.pow(b, 2) - 4*a*c;
-    return [ Math.floor((-b + Math.sqrt(discriminant)) / (2*a) + 1), Math.ceil((-b - Math.sqrt(discriminant)) / (2*a) - 1) ];
+    let sqrtDiscriminant = Math.sqrt(Math.pow(b, 2) - 4*a*c);
+    return [ Math.floor((-b + sqrtDiscriminant) / (2*a) + 1), Math.ceil((-b - sqrtDiscriminant) / (2*a) - 1) ];
 }
