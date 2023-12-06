d1aa2ed87fec58599c1b89d624cab76ebbf84179
mohfez <github@github.com>
2023-12-06 12:41:53 +0000

    day6

--- Day6/Day6.js
+++ Day6/Day6.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
-const PT2 = false;
-let input = fs.readFileSync("Day6/input.txt", { encoding: "utf-8" }).split("\r\n").map(part => PT2 ? part.replace(/\s+/g, "").match(/\d+/g).map(Number) : part.match(/\d+/g).map(Number));
+const PT2 = true;
+const input = fs.readFileSync("Day6/input.txt", { encoding: "utf-8" }).split("\r\n").map(part => PT2 ? part.replace(/\s+/g, "").match(/\d+/g).map(Number) : part.match(/\d+/g).map(Number));
 
 let ans = 1;
 for (let i = 0; i < input[0].length; i++)
@@ -21,7 +21,7 @@ function FindRange(time, distance)
     // (time - heldTime) * heldTime > distance
     // -heldTime^2 + heldTime*time - distance > 0
 
-    let a = -1, b = time, c = -distance;
-    let sqrtDiscriminant = Math.sqrt(Math.pow(b, 2) - 4*a*c);
+    const a = -1, b = time, c = -distance;
+    const sqrtDiscriminant = Math.sqrt(Math.pow(b, 2) - 4*a*c);
     return [ Math.floor((-b + sqrtDiscriminant) / (2*a) + 1), Math.ceil((-b - sqrtDiscriminant) / (2*a) - 1) ];
 }
